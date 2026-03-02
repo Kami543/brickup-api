b65db0505f13cdf2a83a0ee39a6cb085360d9365
Kami543 <witennerrodrigues@gmail.com>
2026-03-02 15:40:12 -0300

    module organization

--- src/organization/organization.module.ts
+++ src/organization/organization.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { OrganizationService } from './organization.service';
 import { OrganizationController } from './organization.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { OrganizationRepository } from './organization.repository';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [OrganizationController],
-  providers: [OrganizationService],
+  providers: [OrganizationService, OrganizationRepository],
 })
 export class OrganizationModule {}
